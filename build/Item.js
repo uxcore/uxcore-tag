e5cf3033365bcba740e30158c3886fefe79c60f3
光弘 <guanghong.wsj@alibaba-inc.com>
2016-06-20 14:23:08 +0800

    ver. 1.0.1

--- build/Item.js
+++ build/Item.js
@@ -8,11 +8,15 @@
 
 "use strict";
 
+function _defaults(obj, defaults) { var keys = Object.getOwnPropertyNames(defaults); for (var i = 0; i < keys.length; i++) { var key = keys[i]; var value = Object.getOwnPropertyDescriptor(defaults, key); if (value && value.configurable && obj[key] === undefined) { Object.defineProperty(obj, key, value); } } return obj; }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
 
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : _defaults(subClass, superClass); }
 
 var React = require('react');
 var ReactDom = require('react-dom');
@@ -107,7 +111,7 @@ var TagItem = function (_React$Component) {
       'li',
       {
         key: props.key,
-        className: classnames("uxcore-tag-item", (_classnames = {}, _classnames[props.className] = !!props.className, _classnames['can-delete'] = props.canDelete, _classnames)) },
+        className: classnames("uxcore-tag-item", (_classnames = {}, _defineProperty(_classnames, props.className, !!props.className), _defineProperty(_classnames, 'can-delete', props.canDelete), _classnames)) },
       React.createElement(
         'span',
         { className: 'uxcore-tag-item-tag', onClick: me.onClick.bind(me, props.tag) },
